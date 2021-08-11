5b3cc594cfb3fd357f58864b2f8c5c097e161c7c
arifsefrianto <arifsefrianto@gmail.com>
2021-08-12 00:00:34 +0700

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -886,6 +886,24 @@ map("v", "<Leader>:", ":Tabularize /:\zs<CR>")
 map("n", "<Leader>>", ":Tabularize /=><CR>")
 map("v", "<Leader>>", ":Tabularize /=><CR>")
 
+map('n', '<C-h>', [[<C-\><C-n><C-W>h]])
+map('n', '<C-j>', [[<C-\><C-n><C-W>j]])
+map('n', '<C-k>', [[<C-\><C-n><C-W>k]])
+map('n', '<C-l>', [[<C-\><C-n><C-W>l]])
+
+function _G.set_terminal_keymaps()
+  local opts = {noremap = true}
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
 opt.foldmethod="expr"
 opt.foldexpr="nvim_treesitter#foldexpr()"
 
