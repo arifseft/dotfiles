ca2304dcad7c57dc83d6b81cac0196f44dc20f80
arifsefrianto <arifsefrianto@gmail.com>
2021-08-15 21:13:02 +0700

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -59,7 +59,7 @@ g.mapleader = ","
 -- Plugins
 require("packer").startup(
   function()
-    use "akinsho/nvim-toggleterm.lua"
+    -- use "akinsho/nvim-toggleterm.lua"
     -- use "alvan/vim-closetag"
     -- use "arcticicestudio/nord-vim"
     use "b3nj5m1n/kommentary"
@@ -77,8 +77,6 @@ require("packer").startup(
     use "hrsh7th/vim-vsnip-integ"
     use "jiangmiao/auto-pairs"
     use "jwalton512/vim-blade"
-    use "kristijanhusak/vim-dadbod-completion"
-    use "kristijanhusak/vim-dadbod-ui"
     use "kyazdani42/nvim-web-devicons"
     use "kyazdani42/nvim-tree.lua"
     use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'}}
@@ -98,11 +96,14 @@ require("packer").startup(
     -- use "rbgrouleff/bclose.vim"
     use "rmagatti/auto-session"
     use "terryma/vim-multiple-cursors"
-    use "tpope/vim-dadbod"
     use "tpope/vim-repeat"
     -- use "tpope/vim-surround"
     -- use "wellle/targets.vim"
     use "wbthomason/packer.nvim"
+
+    -- use "tpope/vim-dadbod"
+    -- use "kristijanhusak/vim-dadbod-completion"
+    -- use "kristijanhusak/vim-dadbod-ui"
 
     use {
       "folke/trouble.nvim",
@@ -204,17 +205,17 @@ require("auto-session").setup(
   }
 )
 
-require("toggleterm").setup {
-  open_mapping = [[<c-\>]],
-  hide_numbers = true, -- hide the number column in toggleterm buffers
-  shade_filetypes = {},
-  shade_terminals = false,
-  start_in_insert = true,
-  insert_mappings = true, -- whether or not the open mapping applies in insert mode
-  persist_size = true,
-  close_on_exit = true, -- close the terminal window when the process exits
-  shell = vim.o.shell, -- change the default shell
-}
+-- require("toggleterm").setup {
+--   open_mapping = [[<c-\>]],
+--   hide_numbers = true, -- hide the number column in toggleterm buffers
+--   shade_filetypes = {},
+--   shade_terminals = false,
+--   start_in_insert = true,
+--   insert_mappings = true, -- whether or not the open mapping applies in insert mode
+--   persist_size = true,
+--   close_on_exit = true, -- close the terminal window when the process exits
+--   shell = vim.o.shell, -- change the default shell
+-- }
 
 -- -- formatter
 -- local prettier = function()
@@ -709,7 +710,7 @@ require "compe".setup {
     nvim_lua = true,
     vsnip = true,
     luasnip = true,
-    vim_dadbod_completion = true
+    -- vim_dadbod_completion = true
   }
 }
 
@@ -955,23 +956,23 @@ map("v", "<Leader>:", ":Tabularize /:\zs<CR>")
 map("n", "<Leader>>", ":Tabularize /=><CR>")
 map("v", "<Leader>>", ":Tabularize /=><CR>")
 
-map('n', '<C-h>', [[<C-\><C-n><C-W>h]])
-map('n', '<C-j>', [[<C-\><C-n><C-W>j]])
-map('n', '<C-k>', [[<C-\><C-n><C-W>k]])
-map('n', '<C-l>', [[<C-\><C-n><C-W>l]])
+-- map('n', '<C-h>', [[<C-\><C-n><C-W>h]])
+-- map('n', '<C-j>', [[<C-\><C-n><C-W>j]])
+-- map('n', '<C-k>', [[<C-\><C-n><C-W>k]])
+-- map('n', '<C-l>', [[<C-\><C-n><C-W>l]])
 
-function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
-end
+-- function _G.set_terminal_keymaps()
+--   local opts = {noremap = true}
+--   vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+--   vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
+--   vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+--   vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+--   vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+--   vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+-- end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+-- vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 opt.foldmethod="expr"
 opt.foldexpr="nvim_treesitter#foldexpr()"
